220d2e97c179abcb6baff25eb933189f8a1f091c
valder72 <mladshenki@gmail.com>
2025-09-20 14:29:19 +0300

    Add files via upload

---
+++ FidoCode.py
@@ -0,0 +1,20 @@
+n = input("Choose position: ")
+while True:
+   try:
+       n = int(n)
+       break
+   except ValueError:
+       n = input("Choose position: ")
+
+if n <= 1:
+    element = None
+    print("No elements found")
+else:
+    element = int((2 * n)/ 3) - 1
+    print(f"Element located in position {n} for (2 * n)/ 3) - 1 is {element}")
+
+
+
+
+
+
